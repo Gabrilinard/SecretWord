4e010cb2611a2d11c123e0a346c07d6f4b81fa6d
Gabriel Linard <127212969+Gabrilinard@users.noreply.github.com>
2024-02-22 13:26:10 -0300

    Enviando meu deploy

---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capo", "Pneu", "Antena", "Retrovisor", "Embreagem", "Vidro", "Banco", "Buzina", "Cambio", "Pedal"],
+    fruta: ["Banana", "Maça", "Pera", "Mamao", "Laranja"],
+    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos", "Barriga", "Pe", "Coxa", "Ombro", "Mao", "Dedo"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Placa mae"],
+    programação: ["Linguagem", "Framework", "JavaScript", "React", "Css", "Java", "Python", "Html", "Frontend", "Backend", "TypeScript"],
+    alimento: ["Arroz", "Feijao", "Carne", "Leite", "Ovo", "Macarrao", "Batata", "Queijo", "Presunto", "Pao", "Cacau"],
+  };
